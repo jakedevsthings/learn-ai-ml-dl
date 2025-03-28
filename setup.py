2f1fcd355a40f4cf6bf4f5763700c01c51adeb3a
jakedevsthings <jacob.e.siau@gmail.com>
2025-03-28 04:14:03 -0500

    redo dir structure for modularity for future milestones/projects

--- setup.py
+++ setup.py
@@ -9,5 +9,12 @@ setup(
         "phase0.calculus",
         "phase0.statistics",
         "phase0.python_optimization",
+        "phase1",
+        "phase1.milestone_1_1_trivial_nn",
+        "phase1.milestone_1_1_trivial_nn.models",
+        "phase1.milestone_1_1_trivial_nn.train",
+        "phase1.milestone_1_1_trivial_nn.utils",
+        "phase1.milestone_1_1_trivial_nn.notebooks",
+        "phase1.milestone_1_1_trivial_nn.tests",
     ]),
 )
