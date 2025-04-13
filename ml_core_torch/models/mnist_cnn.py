eef0810e3d5e0692a3e60cabe3d53d613e904c94
jakedevsthings <jacob.e.siau@gmail.com>
2025-04-13 05:17:43 -0500

    Increase intermediate channels in CNN

--- ml_core_torch/models/mnist_cnn.py
+++ ml_core_torch/models/mnist_cnn.py
@@ -13,16 +13,17 @@ class MNISTCNN(nn.Module):
         super().__init__()
 
         self.net = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=16, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Flatten(),
-            nn.Linear(in_features=32 * 7 * 7, out_features=128),
+            nn.Linear(in_features=64 * 7 * 7, out_features=128),
             nn.ReLU(),
             nn.Linear(in_features=128, out_features=10),
+            
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
